ac50bbba31e7fce87fa385e246587c7211519b43
8x16 <onedropsync@gmail.com>
2023-04-29 15:50:08 +0530

    Add Next Button to the Page

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,15 @@
-console.log('Hello Advanced User, \nPlease feel to give us any suggestions or contribute on github');
+console.log(
+  "Hello Advanced User, \nPlease feel to give us any suggestions or contribute on github"
+);
+
+// const nextBtn = document.getElementById("nextBtn"); - no need for this
+
+nextBtn.addEventListener("click", () => {
+  document.location.reload();
+});
 
 /* To Fetch From an API */
-const typeFitApiURL = "https://type.fit/api/quotes"
+const typeFitApiURL = "https://type.fit/api/quotes";
 let apiQuotes;
 
 //We can get data in 2 ways: 1-> fetch & 2 -> promise
